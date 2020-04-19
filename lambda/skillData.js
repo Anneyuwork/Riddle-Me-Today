7497cb82a603dbc93e08b1bbbe7b135729fae9d0
Anneyuwork <anne.yu.work@gmail.com>
2020-04-18 23:03:58 -0700

    Day 3 : get user answer, set session attribute, use S3 to give return welcome

---
+++ lambda/skillData.js
@@ -0,0 +1,28 @@
+module.exports =  {
+    "en-US":{
+        "WELCOME_MESSAGE":"Welcome, to Riddle Me Today. The Less chances you take, the more points you win.",
+        "QUESTION":"Are you ready for today's riddle?"
+    },
+    "en-UK":{
+        "WELCOME_MESSAGE":"Welcome, to Riddle Me Today. The Less chances you take, the more points you win.",
+        "QUESTION":"Are you ready for today's riddle?"
+    },
+    "en-GB":{
+        "WELCOME_MESSAGE":"Welcome, to Riddle Me Today. I am going to ask you a riddle every day, and 3 chances to answer it correctly. The Less chances you take, the more points you win. ",
+        "REYURNING_USERS_WELCOME":"Welcome back to Riddle Me Today! ",
+        "QUESTION":"Are you ready for today's riddle?",
+        "ANSWER_MESSAGE":"Do you know the answer for this?",
+        "QUESTIONS": [
+            "What has many keys but can't open a single lock?",
+            "What can travel around the world while staying in a corner?",
+            "The eight of us go forth nor back",
+            "what can you catch but never throw?"
+            ],
+        "ANSWERS": [
+            "Piano",
+            "Stamp",
+            "Pawns",
+            "Clod"
+            ]
+    }
+}
